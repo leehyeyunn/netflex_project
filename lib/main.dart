b76efe28da8569df55dc0c14faa9fd67589bd90d
leehyeyun <ihyeyun1022@gmail.com>
2024-04-01 09:02:35 +0900

    잔디 안심어져서 다시 push

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BbongFlix',
+      title: 'hyleeFlix',
       theme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: Colors.black,
