b51e6e8660f5b52e3db5d957be07af7f8ea06ac7
leehyeyun <ihyeyun1022@gmail.com>
2024-04-02 08:54:06 +0900

    상단 navigation bar 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -46,7 +46,7 @@ class TopBar extends StatelessWidget {
           Container(
             padding: EdgeInsets.only(right: 1),
             child: Text(
-              '내가 찜한 콘텐츠',
+              '내가 찜한 콘텐츠 ',
               style: TextStyle(fontSize: 14),
             ),
           ),
